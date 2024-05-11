5ae0eb428309985e76d424841e62cf12f848887c
Kiril Peyanski <peyanski@progress.com>
2024-05-11 13:26:56 +0300

    WIP(fluent): add design

---
+++ apps/docs/src/app/demos/fluent-2/button/page.tsx
@@ -0,0 +1,11 @@
+import { BakaButton } from "baka-ui";
+
+export default function ButtonDemo() {
+  return (
+    <>
+      <div className="flex flex-col">
+        <BakaButton>Button</BakaButton>
+      </div>
+    </>
+  );
+}
---
+++ apps/docs/src/app/demos/fluent-2/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/app/demos/fluent-2/**/*.{js,ts,jsx,tsx,mdx}"],
+  important: "#fluent-2--demos",
+//   presets: [require("baka-material-you/tailwind")],
+  theme: {},
+  plugins: [],
+};
